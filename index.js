96c4dc7928e234893d74214d882e6a84b41fcae4
minas mike <menelikmichael@gmail.com>
2023-10-24 08:17:11 +0300

    finished

--- index.js
+++ index.js
@@ -1,99 +1,126 @@
 function getComputerChoice() {
     const choiceList = ['rock', 'paper', 'scissors'];
     const choiceInt = Math.floor(Math.random() * 3);
-    //console.log(choiceInt)
     return choiceList[choiceInt]
 }
-
-let computerPoint = 0
-let playerPoint = 0
-function playRound(playerSelection, computerSelection) {
-    ps = playerSelection.toLowerCase();
-    cs = computerSelection;
-    if(ps == 'rock' || ps == 'paper' || ps == 'scissors'){
+  let computerPoint = 0
+  let playerPoint = 0
+  function playRound(playerSelection, computerSelection) {
+      console.log('player choice: '+ playerSelection)
+      console.log('computer chioce: ' + computerSelection)
+      ps = playerSelection.toLowerCase();
+      cs = computerSelection;
+      if(ps == 'rock' || ps == 'paper' || ps == 'scissors'){
     
-    if(cs == 'rock'){
-        switch(ps) {
-            case 'rock':
-                console.log('Tie!')
-                break
+      if(cs == 'rock'){
+          switch(ps) {
+              case 'rock':
+                  console.log('Tie!')
+                  bodyMessage('Tie')
+                  break
 
-            case 'paper':
-                console.log('player wins!')
-                playerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
-            case 'scissors':
-                console.log('computer wins!')
-                computerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
-        }
+              case 'paper':
+                  console.log('player wins!')
+                  playerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('player wins')
+                  winnerAlert()
+                  break
+              case 'scissors':
+                  console.log('computer wins!')
+                  computerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('computer wins')
+                  winnerAlert()
+                  break
+          }
 
-    }else if(cs== 'paper') {
-        switch(ps) {
-            case 'rock':
-                console.log('computer wins!')
-                computerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
-            case 'paper':
-                console.log('Tie!')
-                break
-            case 'scissors':
-                console.log('player wins!')
-                playerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
-        }
+      }else if(cs== 'paper') {
+          switch(ps) {
+              case 'rock':
+                  console.log('computer wins!')
+                  computerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('computer wins')
+                  winnerAlert()
+                  break
+              case 'paper':
+                  console.log('Tie!')
+                  bodyMessage('Tie')
+                  break
+              case 'scissors':
+                  console.log('player wins!')
+                  playerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('player wins')
+                  winnerAlert()
+                  break
+          }
 
-    } else if(cs == 'scissors'){
-        switch(ps) {
-            case 'rock':
-                console.log('player wins!')
-                playerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
+      } else if(cs == 'scissors'){
+          switch(ps) {
+              case 'rock':
+                  console.log('player wins!')
+                  playerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('player wins')
+                  winnerAlert()
+                  break
 
-            case 'paper':
-                console.log('computer wins!')
-                computerPoint += 1
-                console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-                break
+              case 'paper':
+                  console.log('computer wins!')
+                  computerPoint += 1
+                  console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+                  bodyMessage('computer wins')
+                  winnerAlert()
+                  break
 
-            case 'scissors':
-                console.log('Tie!')
-                break
-        }
+              case 'scissors':
+                  console.log('Tie!')
+                  bodyMessage('Tie')
+                  break
+          }
 
-    }}else {
-        console.log('invalid input \n please enter rock, paper or scissors only!')
-    }
+      }}else {
+          console.log('invalid input \n please enter rock, paper or scissors only!')
+      }
+  }
+
+  function rockBtnfn(){  
+    document.getElementById('results').innerHTML = 'rock'
+    playerSelection = 'rock'
+    computerSelection = getComputerChoice();
+    playRound('rock', computerSelection)
+}
+function paperBtnfn(){
+    document.getElementById('results').innerHTML = 'Paper'
+    playerSelection = 'paper'
+    computerSelection = getComputerChoice();
+    playRound('paper', computerSelection)
+}
+function scissorsBtnfn(){ 
+    document.getElementById('results').innerHTML = 'Scissors'
+    playerSelection = 'scissors'
+    computerSelection = getComputerChoice();
+    playRound('scissors', computerSelection)
 }
 
-
-
-
-
-function game() {
-    
-    for(i =0; i< 5; i++){
-        let playerSelection = prompt('enter rock, paper or scissors.')
-        let computerSelection = getComputerChoice();
-        console.log('player choice: '+ playerSelection)
-        console.log('computer chioce: ' + computerSelection)
-
-        playRound(playerSelection, computerSelection)
+function winnerAlert(){
+    if(computerPoint == 5){
+        alert('computer wins')
+        playerPoint = 0
+        computerPoint = 0
     }
-    if(playerPoint > computerPoint){
-        console.log('player wins the  game')
-        console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-    } else if(playerPoint < computerPoint){
-        console.log('computer wins the  game')
-        console.log(playerPoint + ' player : ' + computerPoint + ' computer')
-    }else{
-        console.log('Tie game')
-        console.log(playerPoint + ' player : ' + computerPoint + ' computer')
+    else if (playerPoint == 5) {
+        alert('you win')
+        playerPoint = 0
+        computerPoint = 0
+        
     }
 }
-game()
+function bodyMessage(winner){
+    document.getElementById('player').innerHTML = "player choice: " + playerSelection
+    document.getElementById('computer').innerHTML = "computer choice: " + computerSelection
+    document.getElementById('winner').innerHTML = winner
+    document.getElementById('results').innerHTML = playerPoint + ' player : ' + computerPoint + ' computer'
+}
